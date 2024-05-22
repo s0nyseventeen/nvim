af27722ca004214cce8ff10f08cdcff1758a4076
503282404 <stanislav.kryshchuk@gehealthcare.com>
2024-05-22 17:15:22 +0300

    sony@G45TQ4D3E

--- lua/vim-editor-options.lua
+++ lua/vim-editor-options.lua
@@ -4,7 +4,10 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd('abbr bk breakpoint()')
 
+-- Set case-insensitive search
 vim.o.ignorecase = true
+vim.o.smartcase = true
+
 
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap('n', '<leader>t', ':tabe<CR>', {})
