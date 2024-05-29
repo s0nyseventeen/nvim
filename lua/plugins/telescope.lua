5e22007a4a3fcaeb50b2afa2303c4f3904842310
503282404 <stanislav.kryshchuk@gehealthcare.com>
2024-05-29 13:03:50 +0300

    sony@G45TQ4D3E

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,24 @@ return {
   tag = '0.1.6',
   dependencies = {'nvim-lua/plenary.nvim'},
   config = function()
+    local telescope = require("telescope").setup {
+      defaults = {
+        file_ignore_patterns = {
+          '.git',
+          '.vscode',
+          '.idea',
+          '.cache',
+          '.coverage',
+          '.pytest_cache',
+          '.mypy_cache',
+          '__pycache__',
+          'node_modules',
+          'venv',
+          'env',
+          'Virtual Box VMs',
+        }
+      }
+    }
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
