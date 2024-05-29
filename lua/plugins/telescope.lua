8f453123da4d681c32d270bc4e60c19c7400bef3
503282404 <stanislav.kryshchuk@gehealthcare.com>
2024-05-29 12:44:44 +0300

    sony@G45TQ4D3E

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,5 +6,6 @@ return {
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>b', builtin.buffers, {})
   end
 }
