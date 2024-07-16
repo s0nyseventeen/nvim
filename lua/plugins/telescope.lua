e930c899a9ece4557c6b5eb4d7d2e66d5b79d6df
Stanislav Kryshchuk <503282404@gehealthcare.com>
2024-07-16 16:19:57 +0300

    sony@dell

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,48 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    {
+      "<leader>fp",
+      function()
+        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+      end,
+      desc = "Find Plugin File",
+    },
+  },
+  opts = {
+    defaults = {
+      layout_strategy = "horizontal",
+      layout_config = { prompt_position = "bottom" },
+      sorting_strategy = "ascending",
+      winblend = 0,
+      vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--glob=!package.json",
+        "--glob=!package-lock.json",
+      },
+      file_ignore_patterns = {
+        ".git",
+        ".vscode",
+        ".idea",
+        ".cache",
+        ".coverage",
+        ".mypy_cache",
+        ".pytest_cache",
+        "__pycache__",
+        "node_modules",
+        "venv",
+        "env",
+        "Virtual Box VMs",
+        "%.png",
+        "%.jpg",
+        "%.jpeg",
+      },
+    },
+  },
+}
