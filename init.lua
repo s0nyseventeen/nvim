ceb62ce1efbda0c037ef872d6cc49486905abc52
Stanislav Kryshchuk <stanislav_kryshchuk@epam.com>
2026-03-27 18:35:11 +0200

    sony@dell

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "<leader>j", "j$", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>k", "k$", { noremap = true, silent = true })
 
 -- open config
-vim.cmd('nmap <leader>c :e ~/.config/nvim/init.lua<cr>')
+vim.cmd('nmap <leader>c :e ~/AppData/Local/nvim/init.lua<cr>')
 
 -- paste without overwriting
 vim.keymap.set('v', 'p', 'P')
