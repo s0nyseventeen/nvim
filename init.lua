3c5c5e5c85066b78077b2269748e6698b99a3128
Stanislav Kryshchuk <503282404@gehealthcare.com>
2026-03-27 09:26:09 +0200

    sony@dell

--- init.lua
+++ init.lua
@@ -1,2 +1,22 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+﻿-- <leader> key
+vim.g.mapleader = ' '
+vim.keymap.set("n", "<leader>j", "j$", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>k", "k$", { noremap = true, silent = true })
+
+-- open config
+vim.cmd('nmap <leader>c :e ~/.config/nvim/init.lua<cr>')
+
+-- paste without overwriting
+vim.keymap.set('v', 'p', 'P')
+
+-- clear search highlighting
+vim.keymap.set('n', '<Esc>', ':nohlsearch<cr>')
+
+-- sync system clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- search ignoring case
+vim.opt.ignorecase = true
+
+-- disable "ignorecase" option if the search pattern contains upper case characters
+vim.opt.smartcase = true
