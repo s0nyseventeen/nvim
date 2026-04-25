0f27936851b95ac467f2eb4b8bae3e74dffe770d
Stanislav Kryshchuk <stanislav_kryshchuk@epam.com>
2026-04-25 14:33:01 +0300

    sony@dell

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,8 +3,9 @@ vim.g.mapleader = ' '
 
 vim.keymap.set("n", "<leader>j", "j$", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>k", "k$", { noremap = true, silent = true })
-
+vim.keymap.set("n", "<leader>d", "dd", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>\\", "obreakpoint()<Esc>", { noremap = true, silent = true })
+vim.cmd('nmap <leader>c :e ~/AppData/Local/nvim/init.lua<cr>')
 
 -- vim.keymap.set({ "i", "x", "n", "s" }, "<F2>", "<cmd>w<cr><esc>", { desc = "Save File" })
 -- vim.keymap.set("n", "<F4>", ":q<CR>", { noremap = true, silent = true })
