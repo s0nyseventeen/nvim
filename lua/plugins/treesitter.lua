bd02911e6751fbb093c4f96757c9af56ddde137d
Stanislav Kryshchuk <stanislav_kryshchuk@epam.com>
2026-04-25 14:52:45 +0300

    sony@dell

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -19,6 +19,34 @@ return {
       vim.keymap.set({ "x", "o" }, "if", function()
         select.select_textobject("@function.inner", "textobjects")
       end)
+
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        select.select_textobject("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        select.select_textobject("@class.inner", "textobjects")
+      end)
+
+      vim.keymap.set({ "x", "o" }, "ai", function()
+        select.select_textobject("@conditional.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ii", function()
+        select.select_textobject("@conditional.inner", "textobjects")
+      end)
+
+      vim.keymap.set({ "x", "o" }, "al", function()
+        select.select_textobject("@loop.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "il", function()
+        select.select_textobject("@loop.inner", "textobjects")
+      end)
+
+      vim.keymap.set({ "x", "o" }, "am", function()
+        select.select_textobject("@call.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "im", function()
+        select.select_textobject("@call.inner", "textobjects")
+      end)
     end,
   },
   { "nvim-treesitter/nvim-treesitter-textobjects" },
